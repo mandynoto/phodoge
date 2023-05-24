e5bbb9e3ffcb3a2b6f1248a679ac0f82ff26db41
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-05-23 22:31:06 -0700

    Feat/call first api (#3)
    * feat(app): create constant for unsplash key
    
    * refactor(.prettierrc): resize tabWidth to 2
    
    * feat(app): get response on photo button search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Search from './components/Search'
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
 const App = () => {
-    const [word, setWord] = useState('')
-    const handleSearchSubmit = (e) => {
-        e.preventDefault()
-        console.log(e.target[0].value)
-    }
-    return (
-        <div>
-            <Header title="kita" />
-            <Search
-                word={word}
-                setWord={setWord}
-                handleSubmit={handleSearchSubmit}
-            />
-        </div>
+  const [word, setWord] = useState('')
+
+  // This is called when pressing search button
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    console.log(word)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div>
+      <Header title="kita" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+    </div>
+  )
 }
 
 export default App
