2f8d4c1fd9dcfdd79730e09cc6954bdbf25de704
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-05-28 20:16:56 -0700

    Feat/structure images (#5)
    * chore(settings): set tabsize to 2
    
    * feat: save images in state
    
    * feat(app): add title field to new image data and set it to user query
    
    * feat(app): don't show any images if there are none searched
    
    * feat(app): hide the text '0' when there are no images
    
    Prefixing images with '!!' makes it a boolean value
    
    * feat(app): loop through images array to create a new one
    
    * feat(app): collect images horizontally
    
    * feat(app): add padding between navbar and images
    
    * feat(app): add vertical padding among images
    
    * chore(app): remove console log images for debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Search from './components/Search'
+import ImageCard from './components/ImageCard'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 const App = () => {
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState('') // stores search word
+  const [images, setImages] = useState([]) // stores images from API
 
   // This is called when pressing search button
   const handleSearchSubmit = (e) => {
@@ -17,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        setImages([{ ...data, title: word }, ...images]) // adds new image (data) to beginning of array
       })
       .catch((err) => {
         console.log(err)
@@ -29,6 +32,15 @@ const App = () => {
     <div>
       <Header title="kita" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((images, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={images} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
