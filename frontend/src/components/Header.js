b868d02337313d3e2accc4eff4f82085845cae60
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-05-29 22:44:13 -0700

    Feat/add logo (#7)
    * feat: delete images
    
    * feat(components): add logo
    
    * feat(header): make logo readable

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Navbar, Container } from 'react-bootstrap'
+import { ReactComponent as Logo } from '../images/logo.svg'
+
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#181D2D',
 }
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
+        <Logo style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
         <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
     </Navbar>
