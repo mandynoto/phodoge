0da7fe885ac3d9c211d108db7e0692e19421e66a
Mandy Noto <mandynoto@icloud.com>
2023-06-05 16:37:59 -0700

    docs(app): simplify descriptions

--- api/app.py
+++ api/app.py
@@ -4,21 +4,14 @@ import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 
-# Define the Unsplash API base URL
-UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"  # is Unsplash API base URL
 
-# Load environment variables from a .env.local file in the current directory
-load_dotenv(dotenv_path="./.env.local")
-
-# Get the Unsplash API key from the environment variables
-UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
-
-# Raise an exception if the Unsplash API key is not provided
-if not UNSPLASH_KEY:
+load_dotenv(dotenv_path="./.env.local")  # loads environment variables
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")  # gets Unsplash API key from above
+if not UNSPLASH_KEY:  # raises an error if UNSPLASH_KEY is empty
     raise EnvironmentError("Please create a .env.local file with UNSPLASH_KEY")
 
-# Instantiate a Flask application
-app = Flask(__name__)
+app = Flask(__name__)  # Instantiates a Flask application
 
 # Define a Flask route for "/new-image" to retrieve images based on a query
 @app.route("/new-image")
@@ -29,31 +22,23 @@ def new_image():
     The Unsplash API is then hit with the query parameter to return a random image related to the query.
     """
 
-    # Get the query parameter from the request URL
-    word = request.args.get("query")
-
-    # Define the headers for the Unsplash API request
-    headers = {
+    word = request.args.get("query")  # gets query parameter from request URL
+    headers = {  # defines headers for Unsplash API request
         "Accept-Version": "v1",
         "Authorization": "Client-ID " + UNSPLASH_KEY,
     }
 
-    # Set the query parameters for the Unsplash API request
-    params = {
+    params = {  # sets query parameters for Unsplash API request
         "query": word,
     }
 
     # Make a GET request to the Unsplash API with the headers and parameters
-    # Extract the JSON response from the API
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-    data = response.json()
+    data = response.json()  # extracts JSON data from response
 
-    # Return the JSON data from the Unsplash API
-    return data
+    return data  # that is, the JSON data from Unsplash API
 
 
-# Execute the Flask application if this script is run directly
-# This won't be run if this script is imported as a module
-if __name__ == "__main__":
-    # Run the Flask app on all available network interfaces at port 5050
+if __name__ == "__main__":  # means that when this script is run directly,
+    # run the Flask app on all available network interfaces at port 5050
     app.run(host="0.0.0.0", port=5050)
