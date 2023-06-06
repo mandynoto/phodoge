c15f5db420a422fdbda4551ccb845a214de918d8
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-06-05 18:23:52 -0700

    build: specify exact versions of dependencies (#13)
    Update the Pipfile to use exact versions of dependencies to avoid unexpected behaviors.

--- installapi-source.sh
+++ installapi-source.sh
@@ -7,8 +7,6 @@ install_project() {
 	# Check if the right version of Python is installed
 	if ! command -v python3 >/dev/null || ! python3 -c "import sys; assert sys.version_info[:2] == tuple(map(int, '$python_version'.split('.')[:2]))"; then
 		echo "Python $python_version is not installed. Please install it and run this script again."
-		echo "Press enter to continue"
-		read -r dummy
 		return 1
 	fi
 
@@ -20,15 +18,11 @@ install_project() {
 	echo "*" >>api/.venv/.gitignore
 	cd api || {
 		echo "Failed to change to directory 'api'."
-		echo "Press enter to continue"
-		read -r dummy
 		return 1
 	}
 	echo "Activating the virtual environment..."
 	. .venv/bin/activate || {
 		echo "Failed to activate the virtual environment."
-		echo "Press enter to continue"
-		read -r dummy
 		return 1
 	}
 
@@ -37,53 +31,53 @@ install_project() {
 		echo "Installing pipenv..."
 		python3 -m pip install pipenv || {
 			echo "Failed to install pipenv."
-			echo "Press enter to continue"
-			read -r dummy
 			return 1
 		}
 		export PATH="$HOME/.local/bin:$PATH"
 	fi
 
+	# Create Pipenv environment with specified Python version
+	pipenv --python "$python_version" || {
+		echo "Failed to create Pipenv environment."
+		return 1
+	}
+
 	# Install pip, pipenv and project dependencies
 	echo "Upgrading pip and installing dependencies..."
 	python3 -m pip install --upgrade pip || {
 		echo "Failed to upgrade pip."
-		echo "Press enter to continue"
-		read -r dummy
 		return 1
 	}
-	pip install pipenv || {
-		echo "Failed to install pipenv."
-		echo "Press enter to continue"
-		read -r dummy
+	pipenv install flask=="2.3.2" || {
+		echo "Failed to install Flask."
 		return 1
 	}
-	pipenv install --python "$python_version" flask --dev || {
-		echo "Failed to install project dependencies."
-		echo "Press enter to continue"
-		read -r dummy
+	pipenv install requests=="2.31.0" || {
+		echo "Failed to install requests."
 		return 1
 	}
-	pipenv install --python "$python_version" requests --dev || {
-		echo "Failed to install project dependencies."
-		echo "Press enter to continue"
-		read -r dummy
+	pipenv install python-dotenv=="1.0.0" || {
+		echo "Failed to install python-dotenv."
 		return 1
 	}
-	pipenv install --python "$python_version" python-dotenv --dev || {
-		echo "Failed to install project dependencies."
-		echo "Press enter to continue"
-		read -r dummy
+	pipenv install flask-cors=="3.0.10" || {
+		echo "Failed to install Flask-CORS."
 		return 1
 	}
-	pipenv install --python "$python_version" flask-cors --dev || {
-		echo "Failed to install project dependencies."
-		echo "Press enter to continue"
-		read -r dummy
+	pipenv install --dev black=="23.3.0" || {
+		echo "Failed to install Black."
+		return 1
+	}
+	pipenv install --dev pylint=="2.17.4" || {
+		echo "Failed to install pylint."
+		return 1
+	}
+	pipenv install --dev pycodestyle=="2.10.0" || {
+		echo "Failed to install pycodestyle."
 		return 1
 	}
 
-	echo "Setup complete! You can now start working on the project."
+	echo "API folder created and setup complete!"
 }
 
 install_project
