d0133cc4f2d9fd184da2ef1548bc98dc5a10e06a
Mandy Noto <mandynoto@icloud.com>
2023-06-18 15:37:17 -0700

    style(header): make header follow colorscheme of a doge

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,20 @@ import { Navbar, Container } from 'react-bootstrap'
 import { ReactComponent as Logo } from '../images/logo.svg'
 
 const navbarStyle = {
-  backgroundColor: '#181D2D',
+  backgroundColor: '#fff9e3',
+  height: '2.7rem',
+}
+
+const logoStyle = {
+  maxWidth: '10rem',
+  maxHeight: '2rem',
 }
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo alt={title} style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
+        <Logo alt={title} style={logoStyle} />
         <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
     </Navbar>
